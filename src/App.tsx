41e05f7b882bb1b0b81b39350edfe3b9f60175fa
nj-github <njgithub@web.de>
2019-06-13 21:54:02 +0200

    set up api, searching names works

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
+import { SwapiApi } from './api/swapi-api'
 
 const App: React.FC = () => {
+  let swapiApi: SwapiApi = new SwapiApi();
+  let res = swapiApi.search('r2');
+  res.then((x: any) => console.log(x));
   return (
     <div className="App">
       <h1>swapi namesearch autocomplete</h1>
